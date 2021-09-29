f1da7e1ca1e45e0cd80eed1255554234126f4309
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-09-29 12:52:01 +0200

    add rest api to get pdfs

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import render_template, Flask
+from flask import render_template, send_from_directory, Flask
 from flask_cors import CORS
 import logging
 import argparse
@@ -6,6 +6,11 @@ import os, sys
 
 app = Flask(__name__)
 CORS(app)
+app.config['UPLOAD_FOLDER'] = './static/'
+
+@app.route('/pdf')
+def send_pdf():
+    return send_from_directory(app.config['UPLOAD_FOLDER'], 'lorem-ipsum.pdf')
 
 @app.route('/')
 def home():
